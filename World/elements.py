3294d78d4acdf9b2ddf234859a91f7748f2544aa
Shayan Yousefian <shayan.yousefian1372@gmail.com>
2018-08-03 18:22:42 -0400

    Triangle distance meter added: measures sum of distance of current destination and next one

--- World/elements.py
+++ World/elements.py
@@ -4,6 +4,7 @@ from Grammar.actions import Terminals as T
 
 from enum import Enum
 from typing import List, Dict
+from math import acos, cos, degrees, radians
 
 
 class BaseElement:
@@ -28,17 +29,71 @@ class DistanceMeasures(Enum):
     unreachable = 1000
 
 
-def distance_meter(src: Location, dest: Location) -> DistanceMeasures:
-    distance = ((src.x - dest.x)**2 + (src.y - dest.y)**2)**(1/2)
-
-    if distance <= DistanceMeasures.near.value:
+def filter_distance_enum(raw_distance: float) -> 'DistanceMeasures':
+    if raw_distance <= DistanceMeasures.near.value:
         return DistanceMeasures.near
-    elif distance <= DistanceMeasures.close.value:
+    elif raw_distance <= DistanceMeasures.close.value:
         return DistanceMeasures.close
-    elif distance <= DistanceMeasures.far.value:
+    elif raw_distance <= DistanceMeasures.far.value:
         return DistanceMeasures.far
     else:
         return DistanceMeasures.unreachable
+
+
+def distance(src: Location, dest: Location) -> float:
+    return ((src.x - dest.x)**2 + (src.y - dest.y)**2)**(1/2)
+
+
+def angle(adjacent_1: float, adjacent_2: float, far_hand: float) -> float:
+    """
+    Calculates the angle between adjacent_1 and 2, returning angle between 0 and 180
+    :param adjacent_1:
+    :param adjacent_2:
+    :param far_hand:
+    :return:
+    """
+    if not adjacent_1 or not adjacent_2:
+        return 180.0
+
+    a = adjacent_1
+    b = adjacent_2
+    c = far_hand
+    return degrees(acos((a * a + b * b - c * c)/(2.0 * a * b)))
+
+
+def distance_meter(src: Location, dest: Location) -> DistanceMeasures:
+    return filter_distance_enum(distance(src, dest))
+
+
+def triangle_dist_meter(src: Location, dest: Location, later: Location) -> float:
+    """
+    Custom measuring function measuring distance needs to travel by player to two sequential destinations
+    The function adds penalty distances if the player should go an extra way and come back.
+    :param src: player's current location
+    :param dest: player's next location (the one that is being measured)
+    :param later: player's next designated distance, where has to go after reaching the destination
+    :return: float of a custom distance, if the three points create a perfect triangle,
+    return value is just sum of distance from src to dest + from dest to later.
+    """
+    src_dest = distance(src, dest)
+    src_later = distance(src, later)
+    dest_later = distance(dest, later)
+
+    aggr_dist = src_dest + dest_later
+
+    src_angle = angle(src_dest, src_later, dest_later)
+    later_angle = angle(dest_later, src_later, src_dest)
+
+    src_passed_amount = cos(radians(src_angle)) * src_dest
+    later_passed_amount = cos(radians(later_angle)) * dest_later
+
+    if later_passed_amount < 0:
+        aggr_dist += 2 * (-later_passed_amount)
+
+    if src_passed_amount < 0:
+        aggr_dist += 3 * (-src_passed_amount)
+
+    return aggr_dist
 
 
 class Worthy(BaseElement):
--- run.py
+++ run.py
@@ -2,7 +2,10 @@ from Data import quests
 from Grammar.serializers import write_file
 
 from World.world import World
+from World.elements import triangle_dist_meter
+from World.properties import Location
 
+from math import acos, cos, degrees, radians
 # from GA.ga import *
 #
 #
@@ -10,5 +13,12 @@ from World.world import World
 # for ind in pop[:12]:
 #     print(ind)
 
-world = World()
-world.parse_quest(quest=quests.spy)
+# world = World()
+# world.parse_quest(quest=quests.spy)
+
+source = Location(0, 0)
+candid = Location(-1, -2)
+laterd = Location(10, 0)
+
+meter = triangle_dist_meter(src=source, dest=candid, later=laterd)
+print(meter)
